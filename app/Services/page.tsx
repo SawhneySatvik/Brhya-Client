d9993e5dde16ee9f234a3738c7dee4d05eca68e0
Satvik Sawhney <satvik.sawhney2005@gmail.com>
2024-07-08 15:44:39 +0530

    Added Service page

---
+++ app/Services/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/Services/Hero'
+import ServicesDetailsSection from '../components/Services/ServicesDetailsSection'
+
+export default function page() {
+  return (
+    <>
+    <Hero />
+    <ServicesDetailsSection />
+    </>
+  )
+}
