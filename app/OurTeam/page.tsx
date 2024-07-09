898f9e3efce5367e9fb1e325c6637c55c7af7f07
Satvik Sawhney <satvik.sawhney2005@gmail.com>
2024-07-09 12:51:22 +0530

    OurTeam Folder

---
+++ app/OurTeam/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TeamsSection from '../components/OurTeam/TeamsSection'
+import PassionateTeamSection from '../components/OurTeam/PassionateTeamSection'
+
+export default function page() {
+  return (
+    <>
+    <PassionateTeamSection />
+    <TeamsSection />
+    </>
+  )
+}
