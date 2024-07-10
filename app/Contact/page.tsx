abba44fdd6f06344837c19b15eeff4ac4edc1449
Satvik Sawhney <satvik.sawhney2005@gmail.com>
2024-07-10 18:33:47 +0530

    Contact Page

---
+++ app/Contact/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContactSection from '../components/ContactUs/ContactSection'
+// import ContactDetailsSection from '../components/ContactUs/ContactDetailsSection'
+
+export default function page() {
+  return (
+    <>
+    {/* <ContactDetailsSection /> */}
+    <ContactSection />
+    </>
+  )
+}
+  
